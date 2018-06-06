a298fe1deadc24e469c385ebbe71254fb5cb6c22
bkjo89 email = bkjo89@gmail.com <bkjo89@gmail.com>
2018-06-06 17:08:07 +0900

    Coding UI part2

--- App.js
+++ App.js
@@ -8,21 +8,45 @@ import {
   Dimensions,
   Platform
 } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import ToDo from './ToDo';
 
 const { height, width} = Dimensions.get("window"); // 전체 PAGE SIZE
 
 export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  }
   render() {
+    const {newToDo} = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}> To Do List </Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"} />
+          <TextInput 
+              style={styles.input} 
+              placeholder={"New To Do"} 
+              value={newToDo}
+              onChangeText={this._crontolNewToDo}
+              placeholderTextColor={"#999"}
+              returnKeyType={"done"}
+              autoCorrect={false}
+            />
+            <ScrollView contentContainerStyle={styles.toDos}>
+              <ToDo />
+            </ScrollView>
         </View>
       </View>
     );
   }
+
+  _crontolNewToDo = text => {
+    this.setState({
+      newToDo: text
+    });
+  };
+
 }
 
 const styles = StyleSheet.create({
@@ -59,5 +83,14 @@ const styles = StyleSheet.create({
         
       }
     })
-   }
+  },
+  input: {
+    padding: 20,
+    // borderBottomColor: "#bbb",
+    // borderBottomWidth: 1,
+    fontSize: 25
+  },
+  toDos: {
+    alignItems: "center"
+  }
 });
